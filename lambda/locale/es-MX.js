d7c1df2e1b79e4c67f6127f528aa8dbdca1d2fb6
Nelson <nsaularaujof@gmail.com>
2026-02-11 11:41:36 -0700

    Translate English phrases to Spanish in es-MX.js

---
+++ lambda/locale/es-MX.js
@@ -0,0 +1,19 @@
+// locale/es-MX.js
+module.exports = {
+  SpeakWelcome: 'Bienvenido a Audiobookshelf, para iniciar, puedes decir "reproduce" seguido del título de un libro para comenzar a escuchar.',
+  SpeakPlaying: 'Reproduciendo',
+  SpeakBy: 'por',
+  SpeakTryAgain: 'Por favor, intenta de nuevo.',
+  SpeakPlaybackBookHandlerNoBookTitle: 'No entendí la solicitud. Intenta decir "reproduce" seguido del título y el autor.',
+  SpeakPlaybackBookHandlerNoBookFoundP1: '"No se encontro',
+  SpeakPlaybackBookHandlerNoBookFoundP2: 'encontrado. Por favor, intenta de nuevo.',
+  SpeakPlayBookIntentHandlerNoBookTitle: 'No entendí la solicitud. Intenta decir "reproduce" seguido del título y el autor".',
+  SpeakPlayBookIntentHandlerNoBookFound: "No pude encontrar un título que coincidiera con:",
+  SpeakUnsupportedAudioIntentHandler: 'Lo siento, todavía no sé hacer eso.',
+  SpeakHelpIntentHandler: 'Intenta decir "reproduce" seguido del título y el autor para empezar a escuchar! ¿Qué escuchamos?',
+  SpeakCancelAndStopIntentHandlerGoodbye: 'Ádios!',
+  SpeakFallbackIntentHandler: 'Lo siento, no entiendo. Inténtalo de nuevo.',
+  SpeakIntentReflectorHandlerP1: 'Acabas de accionar',
+  SpeakIntentReflectorHandlerP2: '',
+  SpeakErrorHandler: 'Lo siento, estoy teniendo problemas para hacer eso. Inténtalo de nuevo.'
+};
